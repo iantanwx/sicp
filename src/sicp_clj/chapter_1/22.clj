15772ae8d0c33bec8fc893984c0a814f7b920212
Ian Tan <ian.tan@go-jek.com>
2020-05-14 10:21:35 +0800

    style: run cljfmt

--- src/sicp_clj/chapter_1/22.clj
+++ src/sicp_clj/chapter_1/22.clj
@@ -8,17 +8,17 @@
 
 (defn next-odd
   [n]
-  (if (even? n) (+ n 1) (+ n 2)) )
+  (if (even? n) (+ n 1) (+ n 2)))
 
 (defn search-for-primes
   [n candidates]
   (let [x (next-odd n)]
     (cond
-        (= (count candidates) 3) candidates
-        (even? n) (search-for-primes x candidates)
-        :else (if (timed-prime-test n)
-          (search-for-primes x (conj candidates n))
-          (search-for-primes x candidates)))))
+      (= (count candidates) 3) candidates
+      (even? n) (search-for-primes x candidates)
+      :else (if (timed-prime-test n)
+              (search-for-primes x (conj candidates n))
+              (search-for-primes x candidates)))))
 
 (search-for-primes 1000, '())
 (search-for-primes 10000, '())
