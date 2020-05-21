3c69e2234f56a8bbe87e66be051e03f42e7c5a3c
Ian Tan <ian.tan@go-jek.com>
2020-05-21 14:24:50 +0800

    fix(ch1): fix solution for prime-product

--- src/sicp_clj/chapter_1/33.clj
+++ src/sicp_clj/chapter_1/33.clj
@@ -1,11 +1,13 @@
 (ns sicp-clj.chapter-1.33
-  (:require [sicp-clj.chapter-1.24 :as ch24]
-            [sicp-clj.chapter-1.28 :as ch28]))
+  (:require
+   [sicp-clj.chapter-1.20 :as ch20]
+   [sicp-clj.chapter-1.24 :as ch24]
+   [sicp-clj.chapter-1.28 :as ch28]))
 
 (defn filtered-acc
   [combiner filter identity-element term a next b]
   (loop [ak a result identity-element]
-    (cond 
+    (cond
       (> ak b) result
       (filter ak) (recur (next ak) (combiner result (term ak)))
       :else (recur (next ak) (combiner result identity-element)))))
@@ -18,5 +20,5 @@
 
 (defn prime-product
   [n]
-  (filtered-acc * ch28/miller-rabin 1 identity 1 inc (- n 1)))
-(prime-product 5)
+  (filtered-acc * #(= 1 (ch20/gcd n %1)) 1 identity 1 inc n))
+(prime-product 10)
