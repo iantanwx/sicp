42f56cdb4c86a27905d262fe007c81b0ab9c3942
Ian Tan <ian.tan@go-jek.com>
2020-05-21 13:57:30 +0800

    feat(ch1): implement ex1.33

---
+++ src/sicp_clj/chapter_1/33.clj
@@ -0,0 +1,22 @@
+(ns sicp-clj.chapter-1.33
+  (:require [sicp-clj.chapter-1.24 :as ch24]
+            [sicp-clj.chapter-1.28 :as ch28]))
+
+(defn filtered-acc
+  [combiner filter identity-element term a next b]
+  (loop [ak a result identity-element]
+    (cond 
+      (> ak b) result
+      (filter ak) (recur (next ak) (combiner result (term ak)))
+      :else (recur (next ak) (combiner result identity-element)))))
+
+(defn sum-prime-squares
+  [a b]
+  (filtered-acc + ch28/miller-rabin 0 ch24/square a inc b))
+
+(assert (= (sum-prime-squares 1 5) (+ 1 4 9 25)))
+
+(defn prime-product
+  [n]
+  (filtered-acc * ch28/miller-rabin 1 identity 1 inc (- n 1)))
+(prime-product 5)
