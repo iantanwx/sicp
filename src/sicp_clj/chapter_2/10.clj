9da439e4685953c8bea3876bd06f4d2fdea8609f
Ian Tan <ian.tan@go-jek.com>
2020-05-24 15:27:44 +0800

    fix(ch2): fix ex2.10 solution

--- src/sicp_clj/chapter_2/10.clj
+++ src/sicp_clj/chapter_2/10.clj
@@ -1,15 +1,15 @@
 (ns sicp-clj.chapter-2.10
   (:require [sicp-clj.chapter-2.7 :refer :all]))
 
-(defn interval-span
+(defn spans-0?
   [x]
-  (- (upper-bound x) (lower-bound x)))
+  (and (<= (lower-bound x) 0)
+       (>= (upper-bound x) 0)))
 
 (defn div-interval-safe
   "Safely divides the interval x by y"
   [x y]
-  (let [divisor (interval-span y)]
-    (if (= 0 divisor) (throw (IllegalArgumentException. "Cannot divide by an interval of span 0"))
+  (if (spans-0? y) (throw (IllegalArgumentException. "Cannot divide by an interval of span 0"))
         (mul-interval x
                       (make-interval (/ 1.0 (lower-bound y))
-                                     (/ 1.0 (upper-bound y)))))))
+                                     (/ 1.0 (upper-bound y))))))
--- test/sicp_clj/chapter_2/10_test.clj
+++ test/sicp_clj/chapter_2/10_test.clj
@@ -3,7 +3,7 @@
             [sicp-clj.chapter-2.7 :as ex7]
             [sicp-clj.chapter-2.10 :as ex10]))
 
-(deftest test-throws-0
+(deftest test-throws-if-0
   (is (thrown? IllegalArgumentException
                (ex10/div-interval-safe (ex7/make-interval 1.0 3.0)
-                                       (ex7/make-interval 5.0 5.0)))))
+                                       (ex7/make-interval -5.0 5.0)))))
