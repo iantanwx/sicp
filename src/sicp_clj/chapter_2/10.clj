c6fa2071061ea01a4551693bc83fd2cf75615fbb
Ian Tan <ian.tan@go-jek.com>
2020-05-24 15:17:45 +0800

    fix: fix test syntax

--- src/sicp_clj/chapter_2/10.clj
+++ src/sicp_clj/chapter_2/10.clj
@@ -9,7 +9,7 @@
   "Safely divides the interval x by y"
   [x y]
   (let [divisor (interval-span y)]
-    (if (= 0 divisor) (throw (IllegalAccessException. "Cannot divide by an interval of span 0"))
+    (if (= 0 divisor) (throw (IllegalArgumentException. "Cannot divide by an interval of span 0"))
         (mul-interval x
                       (make-interval (/ 1.0 (lower-bound y))
                                      (/ 1.0 (upper-bound y)))))))
--- test/sicp_clj/chapter_2/10_test.clj
+++ test/sicp_clj/chapter_2/10_test.clj
@@ -1,8 +1,9 @@
 (ns sicp-clj.chapter-2.10-test
   (:require [clojure.test :refer :all]
-            [sicp-clj.chapter-2.7 :refer :all]
-            [sicp-clj.chapter-2.10 :refer :all]))
+            [sicp-clj.chapter-2.7 :as ex7]
+            [sicp-clj.chapter-2.10 :as ex10]))
 
 (deftest test-throws-0
-  (is (thrown? (div-interval-safe (make-interval 1.0 3.0)
-                                  (make-interval 5.0 5.0)))))
+  (is (thrown? IllegalArgumentException
+               (ex10/div-interval-safe (ex7/make-interval 1.0 3.0)
+                                       (ex7/make-interval 5.0 5.0)))))
