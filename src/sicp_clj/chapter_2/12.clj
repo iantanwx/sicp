919f636d57e0982674b4199bcb452b7f9439261f
Ian Tan <ian.tan@go-jek.com>
2020-05-24 18:26:28 +0800

    feat(ch2): implement make-center-percent

---
+++ src/sicp_clj/chapter_2/12.clj
@@ -0,0 +1,35 @@
+(ns sicp-clj.chapter-2.12
+  (:require [sicp-clj.chapter-2.7 :refer :all]
+            [clojure.math.numeric-tower :as math]))
+
+(defn make-center-width
+  "Makes an interval with tolerance based on a single number"
+  [c w]
+  (make-interval (- c w) (+ c w)))
+
+(defn make-center-percent
+  "Creates an interval given a center and a percentage tolerance > 0.0 < 1.0"
+  [c p]
+  (make-interval (- c (math/abs (* c p)))
+                 (+ c (math/abs (* c p)))))
+
+(defn center
+  "Gets the originally provided center based on absolute interval"
+  [i]
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(defn width
+  "Gets the width (relative to the center) of the given interval"
+  [i]
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(defn percent
+  "Gets the percentage tolerance relative to the center"
+  [i]
+  (let [w (width i) c (center i)]
+    (math/abs (/ w c))))
+
+(def x (make-center-percent 5 0.1))
+(def y (make-center-percent -5 0.1))
+(percent x)
+(percent y)
