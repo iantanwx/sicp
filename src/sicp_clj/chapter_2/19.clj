53bd3a2ec919684f839a317be45d8e5dc67744ab
Ian Tan <ian.tan@go-jek.com>
2020-05-25 16:52:08 +0800

    feat(ch2.2): count change redux

---
+++ src/sicp_clj/chapter_2/19.clj
@@ -0,0 +1,27 @@
+(ns sicp-clj.chapter-2.19)
+
+(def us-coins [50 25 10 5 1])
+(def uk-coins [100 50 20 10 5 2 1 0.5])
+
+(defn no-more?
+  [coll]
+  (empty? coll))
+
+(defn except-first-denomination
+  [coll]
+  (rest coll))
+
+(defn first-denomination
+  [coll]
+  (first coll))
+
+(defn cc
+  [amount coin-values]
+  (cond
+    (= amount 0) 1
+    (or (< amount 0) (no-more? coin-values)) 0
+    :else (+ (cc amount (except-first-denomination coin-values))
+             (cc (- amount (first-denomination coin-values)) coin-values))))
+
+(cc 100 us-coins)
+(cc 100 uk-coins)
