13c5d6c99466d4deab0d8c4a447c2897c202e728
Ian Tan <ian.tan@go-jek.com>
2020-05-25 16:58:41 +0800

    feat(ch2.2): fix cc redux

--- src/sicp_clj/chapter_2/19.clj
+++ src/sicp_clj/chapter_2/19.clj
@@ -18,7 +18,8 @@
 (defn cc
   [amount coin-values]
   (cond
-    (= amount 0) 1
+    ; gotcha! in Clojure, use == to compare numbers of different categories.
+    (== amount 0) 1
     (or (< amount 0) (no-more? coin-values)) 0
     :else (+ (cc amount (except-first-denomination coin-values))
              (cc (- amount (first-denomination coin-values)) coin-values))))
