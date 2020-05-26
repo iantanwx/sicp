3cef63741d3cfb15cd2362dd60737111720dd65e
Ian Tan <ian.tan@go-jek.com>
2020-05-26 15:37:39 +0800

    feat(ch2.2): use first and rest to pick 7

---
+++ src/sicp_clj/chapter_2/25.clj
@@ -0,0 +1,9 @@
+(ns sicp-clj.chapter-2.25)
+
+(def a '(1 3 (5 7) 9))
+(def b '((7)))
+(def c '(1 (2 (3 (4 (5 (6 7)))))))
+
+(first (rest (first (rest (rest a)))))
+(first (first b))
+(first (rest (first (rest (first (rest (first (rest (first (rest (first (rest c))))))))))))
