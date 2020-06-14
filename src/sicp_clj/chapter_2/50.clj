8cb289a1aeb2e6117f0133feb288db41fac71a42
Ian Tan <iantanwx@bytedance.com>
2020-06-14 18:01:26 +0800

    feat: implement ex 2.50

---
+++ src/sicp_clj/chapter_2/50.clj
@@ -0,0 +1,25 @@
+(ns sicp-clj.chapter-2.50
+  (:require [sicp-clj.pict-lang.core :refer :all]))
+
+(defn flip-horiz
+  [painter]
+  (transform-painter painter
+                     (make-vect 1.0 0.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(defn rotate-180
+  [painter]
+  (transform-painter painter
+                     (make-vect 1.0 1.0)
+                     (make-vect 0.0 1.0)
+                     (make-vect 1.0 0.0)))
+
+(defn rotate-270
+  [painter]
+  (transform-painter painter
+                     (make-vect 0.0 1.0)
+                     (make-vect 0.0 0.0)
+                     (make-vect 1.0 1.0)))
+
+(paint (beside will (rotate-270 will)))
